8fd3c330b3fbdfd7c65f7a85df21668902425d97
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-03-05 22:32:08 +0530

    Add files via upload

---
+++ DoWhileloop1.java
@@ -0,0 +1,12 @@
+class DoWhileloop1
+ {
+  public static void main(String[]args)
+   {
+    int v=1;
+    do
+    {
+     System.out.println(v);
+     v++;
+    }while(v<=10);
+   }
+ }// output is- 1,2,3,4,5,6,7,8,9,10
